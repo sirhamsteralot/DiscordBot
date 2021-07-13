bfdd087b0b04ba1e5e45c76dc910d6abe164f3eb
sirhamsteralot <sirhamsteralot@gmail.com>
2021-07-13 20:24:35 +0200

    eval

---
+++ DiscordBot/Scripting/ScriptManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace DiscordBot.Scripting
+{
+    public static class ScriptManager
+    {
+        static readonly Dictionary<string, AssemblyName> AssemblyNames = new Dictionary<string, AssemblyName>();
+        public static ScriptOptions op = ScriptOptions.Default
+                                  .WithReferences("System.Runtime, Version = 4.0.0.0, Culture = neutral, PublicKeyToken = b03f5f7f11d50a3a")
+                                  .WithReferences(typeof(List<>).Assembly, typeof(Enumerable).Assembly, typeof(string).Assembly, typeof(ScriptManager).Assembly, typeof(StringBuilder).Assembly)
+                                  .WithReferences(SelectAssemblies())
+                                  .WithImports("System", "System.Collections.Generic", "System.Timers", "System.Linq", "System.Text", "HaEPluginCore");
+
+        public static async Task<object> ExecuteScript(string code)
+        {
+            var res = await CSharpScript.EvaluateAsync<object>(code, op);
+            GC.Collect();
+
+            return res?.ToString();
+        }
+
+        private static IEnumerable<Assembly> SelectAssemblies()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic && !string.IsNullOrWhiteSpace(a.Location));
+        }
+    }
+}
---
+++ DiscordBot/Scripting/ScriptingCommands.cs
@@ -0,0 +1,32 @@
+﻿using Discord.WebSocket;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace DiscordBot.Scripting
+{
+    public class ScriptingCommands
+    {
+        public async static Task Eval(SocketMessage message)
+        {
+            string arg = message.Content[(Program.commandManager.CommandStart.Length)..];
+
+            try
+            {
+                var result = await ScriptManager.ExecuteScript(arg);
+                await message.Channel.SendMessageAsync(result?.ToString());
+
+            }
+            catch (CompilationErrorException ex)
+            {
+                string response = $"Error executing script!\n" +
+                        $"```{ex.Message}```";
+
+                await message.Channel.SendMessageAsync(response);
+            }
+        }
+    }
+}
