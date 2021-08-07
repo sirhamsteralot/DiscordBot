5d5b25e37e14cb660fd2ee8e1cc7698858e8afc9
sirhamsteralot <sirhamsteralot@gmail.com>
2021-08-07 14:03:57 +0200

    ability to remove commands

--- DiscordBot/Quoting/QuoteCommands.cs
+++ DiscordBot/Quoting/QuoteCommands.cs
@@ -13,6 +13,7 @@ namespace DiscordBot.Quoting
         public void RegisterCommands(CommandManager commandManager)
         {
             commandManager.AddCommand("quote", QuoteCommand, "gets a quote or adds a quote, usage: quote / quote {quote}");
+            commandManager.AddCommand("removequote", RemoveQuoteCommand, "removes a quote, usage: removequote {quote}");
         }
 
         public async Task QuoteCommand(SocketMessage message)
@@ -36,6 +37,28 @@ namespace DiscordBot.Quoting
             Quote quote = Program.settings.quoteSettings.quotes[r.Next(Program.settings.quoteSettings.quotes.Count)];
 
             await message.Channel.SendMessageAsync(quote.quote);
+        }
+
+        public async Task RemoveQuoteCommand (SocketMessage message)
+        {
+            string argumentPart = message.Content.Substring("quote".Length + Program.settings.systemSettings.commandCode.Length);
+
+            if (argumentPart != "")
+            {
+                int count = Program.settings.quoteSettings.quotes.RemoveAll(x => x.quote == argumentPart);
+
+                if (count > 1)
+                {
+                    await message.Channel.SendMessageAsync($"{count} matching quotes removed!");
+                    return;
+                } else
+                {
+                    await message.Channel.SendMessageAsync("No matching quotes found!");
+                    return;
+                }
+            }
+
+            await message.Channel.SendMessageAsync("Missing Argument!");
         }
     }
 }
