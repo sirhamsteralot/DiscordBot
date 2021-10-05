745d39511ecac4fcad8d9e37f5e5104b78eb117c
sirhamsteralot <sirhamsteralot@gmail.com>
2021-10-05 23:14:23 +0200

    nom an exception

--- DiscordBot/TwitchNotify/TwitchNotifier.cs
+++ DiscordBot/TwitchNotify/TwitchNotifier.cs
@@ -24,50 +24,57 @@ namespace DiscordBot.TwitchNotify
 
         public async void CheckForLive(object state)
         {
-            if (Program.settings.systemSettings.twitchAuthorizationCode == "")
-                return;
-
-            foreach (var channel in Program.settings.twitchSettings.twitchChannels)
+            try
             {
-                string Url = "https://api.twitch.tv/helix/streams?user_login=" + channel.ChannelName;
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                if (Program.settings.systemSettings.twitchAuthorizationCode == "")
+                    return;
 
-                request.Method = "Get";
-                request.Timeout = 5000;
-                request.Headers.Add("Client-ID", "u363xalk2xef1g4miyom7wc7zq3fzn");
-                request.Headers.Add("Authorization", "Bearer " + Program.settings.systemSettings.twitchAuthorizationCode);
-
-                using (var s = request.GetResponse().GetResponseStream())
+                foreach (var channel in Program.settings.twitchSettings.twitchChannels)
                 {
-                    using (var sr = new System.IO.StreamReader(s))
+                    string Url = "https://api.twitch.tv/helix/streams?user_login=" + channel.ChannelName;
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+
+                    request.Method = "Get";
+                    request.Timeout = 5000;
+                    request.Headers.Add("Client-ID", "u363xalk2xef1g4miyom7wc7zq3fzn");
+                    request.Headers.Add("Authorization", "Bearer " + Program.settings.systemSettings.twitchAuthorizationCode);
+
+                    using (var s = request.GetResponse().GetResponseStream())
                     {
-                        bool newStatus = false;
-
-                        var jsonObject = JObject.Parse(sr.ReadToEnd());
-
-                        var theValue = jsonObject.SelectToken("data[0].type");
-                        if (theValue == null)
-                            newStatus = false;
-                        else
-                            newStatus = theValue.ToString() == "live";
-
-                        if (newStatus && channel.LastStatus != newStatus)
+                        using (var sr = new System.IO.StreamReader(s))
                         {
-                            var discordChannel = Program._client.GetChannel(channel.channelId) as ISocketMessageChannel;
+                            bool newStatus = false;
 
-                            if (string.IsNullOrEmpty(channel.MentionId))
-                                await discordChannel.SendMessageAsync($"{channel.ChannelName} went Live!\nhttps://twitch.tv/{channel.ChannelName}");
+                            var jsonObject = JObject.Parse(sr.ReadToEnd());
+
+                            var theValue = jsonObject.SelectToken("data[0].type");
+                            if (theValue == null)
+                                newStatus = false;
                             else
-                                await discordChannel.SendMessageAsync($"<@{channel.MentionId}> {channel.ChannelName} went Live!\nhttps://twitch.tv/{channel.ChannelName}");
+                                newStatus = theValue.ToString() == "live";
+
+                            if (newStatus && channel.LastStatus != newStatus)
+                            {
+                                var discordChannel = Program._client.GetChannel(channel.channelId) as ISocketMessageChannel;
+
+                                if (string.IsNullOrEmpty(channel.MentionId))
+                                    await discordChannel.SendMessageAsync($"{channel.ChannelName} went Live!\nhttps://twitch.tv/{channel.ChannelName}");
+                                else
+                                    await discordChannel.SendMessageAsync($"<@{channel.MentionId}> {channel.ChannelName} went Live!\nhttps://twitch.tv/{channel.ChannelName}");
+                            }
+
+                            channel.LastStatus = newStatus;
+
+                            Program.settings.twitchSettings.RequiresSaving();
+                            Program.settings.SerializeAsync(false);
                         }
-
-                        channel.LastStatus = newStatus;
-
-                        Program.settings.twitchSettings.RequiresSaving();
-                        Program.settings.SerializeAsync(false);
                     }
                 }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
             }
         }
+
     }
 }
